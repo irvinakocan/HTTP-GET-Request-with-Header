9bfc8fa9862c6d48644beed5795d9a801fc018f1
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-12-20 12:44:15 +0100

    Initial Commit

---
+++ HTTP GET Request with Header/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HTTP GET Request with Header
+//
+//  Created by Macbook Air 2017 on 20. 12. 2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
