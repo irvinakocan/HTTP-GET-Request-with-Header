74d239cbe8163c670adeb4197b25194cc32b2c1c
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-12-20 13:44:55 +0100

    HTTP Get Request implementation

--- HTTP GET Request with Header/ViewController.swift
+++ HTTP GET Request with Header/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
+let GET_ENDPOINT = "https://jsonplaceholder.typicode.com/todos/1"
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        getRequest()
     }
 
-
+    func getRequest() {
+        
+        // Create URL
+        guard let url = URL(string: GET_ENDPOINT) else {
+            return
+        }
+        
+        // Create URL Request
+        var urlRequest = URLRequest(url: url)
+        
+        // Specify HTTP Method
+        urlRequest.httpMethod = "GET"
+        
+        // Send HTTP Request
+        let task = URLSession.shared.dataTask(with: urlRequest, completionHandler: {
+            (data, response, error) in
+            
+            if error != nil {
+                print("Error took place.")
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                print("Response HTTP Status code: \(response.statusCode)")
+            }
+        })
+        task.resume()
+    }
 }
 
